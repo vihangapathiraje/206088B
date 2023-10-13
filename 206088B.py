1adf0b22ab612252f029309fc598f2e5f3ff5fb8
username <vihanga.pathiraje30@gmail.com>
2023-10-13 20:26:05 +0530

    My third commit with readme file

--- 206088B.py
+++ 206088B.py
@@ -1,20 +1,25 @@
 import re
+from collections import Counter
 
 def identify_numbers_and_texts(input_string):
     
     number_pattern = r'\d+'
     text_pattern = r'[A-Za-z]+'
 
-   
+    
     numbers = re.findall(number_pattern, input_string)
     texts = re.findall(text_pattern, input_string)
 
-    return numbers, texts
+    
+    number_counts = Counter(numbers)
+    text_counts = Counter(texts)
+
+    return number_counts, text_counts
 
 if __name__ == "__main__":
     input_string = input("Enter a string: ")
-    numbers, texts = identify_numbers_and_texts(input_string)
+    number_counts, text_counts = identify_numbers_and_texts(input_string)
 
-    print("Numbers found: ", numbers)
-    print("Texts found: ", texts)
+    print("Numbers found: ", number_counts)
+    print("Texts found: ", text_counts)
 
